872f5ba2b88bb70f715cbcd3e53e01d83beccc64
Lim Yoong Wah <limyoongwah90@gmail.com>
2015-03-01 18:05:13 +0800

    update index and post

--- models/Posts.js
+++ models/Posts.js
@@ -6,4 +6,8 @@ var PostSchema = new mongoose.Schema({
 	comments:[{type:mongoose.Schema.Types.ObjectId,ref:'Comment'}]
 
 });
+PostSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
 mongoose.model('Post',PostSchema);
--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,43 @@ router.post('/posts', function(req, res, next) {
     res.json(post);
   });
 });
+router.param('post',function(req,res,next,id){
+	var query = Post.findById(id);
+	query.exec(function(err,post){
+		if (err) { return next(err); }
+		if (!post) { return next(new Error('cannot find post'));}
+
+		req.post = post;
+		return next();
+	});
+});
+router.get('/posts/:post',function(req,res){
+	 req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+router.put('/posts/:post/upvote',function(req,res,next){
+	req.post.upvote(function(err,post){
+		if(err){return next(err)};
+		res.json(post);
+	});
+});
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
 
 module.exports = router;
